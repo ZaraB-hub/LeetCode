7fc5f3b42e01d364a61fa5d5ae0190315e8845a4
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2022-10-12 10:19:42 +0200

    Time: 2 ms (82.72%), Space: 52.1 MB (67.70%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+     int sum=0;
+     for(int i=0;i<nums.length;i++){
+         sum+=nums[i];
+     }
+        
+
+    int leftsum=0;
+    int rightsum;
+    
+        
+    for(int i=0;i<nums.length;i++){
+    rightsum=sum-leftsum-nums[i];
+        
+     if(leftsum==rightsum)
+         return i;
+        leftsum+=nums[i];
+    }
+        return -1;
+        
+        
+    }
+    }
