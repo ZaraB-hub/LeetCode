bfc4af75d7754d08d37e300a6f16686bf608603d
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2022-10-12 09:44:45 +0200

    Time: 1 ms (41.71%), Space: 43.3 MB (45.95%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         
-     int helper=nums[0];
-        
-      int[]result=new int[nums.length];
-      result[0]=nums[0];
+     
       
-      for(int i=1;i<result.length;i++){
-    
-          result[i]=helper+nums[i];
-          helper+=nums[i];
+      for(int i=1;i<nums.length;i++){
           
-          // result[i] = result[i - 1] + nums[i];
+          nums[i]=nums[i-1]+nums[i];
+    
       }
     
 
     
-   return result;
+   return nums;
         
         
         
