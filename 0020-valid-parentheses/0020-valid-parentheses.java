85792690ef63ffd780be284cf028cae2f91179c1
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2023-01-04 22:42:08 +0100

    Time: 4 ms (44.21%), Space: 41.9 MB (52.30%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        char arr[] = s.toCharArray();
+		Stack<Character> set = new Stack();
+		for (Character ch : arr) {
+			if (ch == '{' || ch == '[' || ch == '(') {
+				set.push(ch);
+			} else if (ch == ']') {
+				if (set.isEmpty() || set.peek() != '[') {
+					return false;
+				} 
+				set.pop();
+			} else if (ch == ')') {
+				if (set.isEmpty() ||  set.peek() != '(') 
+					return false;
+				set.pop();	
+			} else if (ch == '}') {
+				if (set.isEmpty() ||  set.peek() != '{') 
+					return false;
+				set.pop();	
+			}
+		}
+		return set.size() == 0;
+	}
+}
