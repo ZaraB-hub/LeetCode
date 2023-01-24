9c56eae1fb4452d016adbb940968f5ad07b2280f
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2023-01-24 23:12:01 +0100

    Time: 1 ms (36.74%), Space: 41.6 MB (5.62%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -4,14 +4,11 @@ class Solution {
     int counter=0;
         
      while(num!=0){   
-        if(num%2==0){
-            num=num/2;
-            counter++;
-        }  
-        else {
+        if(num%2==0)
+            num/=2;
+        else 
             num--;
-            counter++;
-        }
+         counter++;
      }
         return counter;
     }
