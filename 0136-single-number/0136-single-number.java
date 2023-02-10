d7f227ac5252047a4f5d79326382ed137d72da8e
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2023-02-10 12:56:14 +0100

    Time: 10 ms (32.32%), Space: 43.3 MB (25.72%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> table = new HashMap<>();
+        for (int num : nums) {
+            table.put(num, table.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int num : nums) {
+            if (table.get(num) == 1) {
+                return num;
+            }
+        }
+        return 0;
+    }
+}
