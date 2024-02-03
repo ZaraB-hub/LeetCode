6d4a7954ca6266c40911231496ee781e342a3a0b
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2024-02-03 15:53:29 +0100

    Time: 2 ms, Memory: 45.5 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        
+        int i=0;
+        int track1=0;
+        int track2=0;
+
+        while (i < arr.length - 1 && arr[i] < arr[i + 1]) {
+            track1++;
+            i++;
+        }
+
+        while (i < arr.length - 1 && arr[i] > arr[i + 1]) {
+            track2++;
+            i++;
+        }
+        if (track1 > 0 && track2 > 0 && i == arr.length - 1) {
+            return true;
+        }
+
+        return false;
+}
+}
