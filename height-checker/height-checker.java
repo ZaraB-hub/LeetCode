983e3cf2dbe9e6655f7ac97de1f849c58b513614
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2024-02-04 20:10:18 +0100

    Time: 2 ms, Memory: 41.6 MB - LeetHub

---
+++ height-checker/height-checker.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int[] expected=new int[heights.length];
+        
+        int k=0;
+        
+        for(int i=0;i<heights.length;i++){
+            expected[i]=heights[i];
+            }
+        Arrays.sort(expected);
+
+    
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i])
+                k++;
+        }    
+
+    
+    
+    return k;
+        }
+    
+    
+}
