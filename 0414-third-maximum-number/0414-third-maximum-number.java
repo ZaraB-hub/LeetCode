f48c1fd00f993c172752602b9f754efd3efa0346
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2024-02-05 17:19:40 +0100

    Time: 8 ms (14.05%), Space: 44.7 MB (9.63%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        Arrays.sort(nums);
+        
+//         int helper;
+//         for(int i=0;i<nums.length/2;i++){
+//             helper=nums[i];
+//             nums[i]=nums[nums.length-i-1];
+//             nums[nums.length-i-1]=helper;
+//         }       
+        
+        //remove duplicates
+        
+    Set<Integer> set = new HashSet<Integer>();
+    for (int num : nums) {
+    set.add(num);
+}
+    int n = set.size(); 
+    int arr[] = new int[n]; 
+    
+        int z = 0; 
+        for (Integer x : set) 
+            arr[z++] = x; 
+        
+ Arrays.sort(arr);
+
+    int max=arr[0];    
+    int max2=arr[0];
+    int max3=arr[0];
+     for(int na:arr){
+         if (max<na){
+             max3=max2;
+             max2=max;
+             max=na;
+         }
+         else if (na > max2 && na != max) {
+                max3 = max2;
+                max2 = na;
+            } else if (na > max3 && na != max2 && na != max) {
+                max3 = na;
+            }
+     }
+        if(arr.length<3)
+            return max;
+        else
+            return max3;
+    }
+}
