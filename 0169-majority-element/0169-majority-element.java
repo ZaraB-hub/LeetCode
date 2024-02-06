6e38114d23410f8cd19cf9303ae37ef99827deb8
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2024-02-06 23:24:30 +0100

    Time: 1 ms (99.81%), Space: 50.5 MB (35.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,16 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-      HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
-        
-     for(int num:nums){
-         if(numbers.get(num)!=null){
-             numbers.put(num,numbers.get(num)+1);
-         }else
-             numbers.put(num,1);
-             
-     }
-    
-    int k=0;
-    for (Map.Entry<Integer, Integer> entry : numbers.entrySet()) {
-    if(entry.getValue()>nums.length/2){
-        k=entry.getKey();
-        break;
-    }
-}
+public class Solution {
+    public int majorityElement(int[] num) {
 
-        return k;
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
     }
 }
