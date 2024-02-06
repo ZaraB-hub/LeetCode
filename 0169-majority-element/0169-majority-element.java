b2e66fbeee5707f6eaf9a11ca53be93f50913fdc
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2024-02-06 23:25:32 +0100

    Time: 4 ms (55.08%), Space: 51.2 MB (5.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,7 @@
-public class Solution {
-    public int majorityElement(int[] num) {
-
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
-                count++;
-                major=num[i];
-            }else if(major==num[i]){
-                count++;
-            }else count--;
-            
-        }
-        return major;
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
     }
 }
