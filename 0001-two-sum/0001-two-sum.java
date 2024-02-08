b423e2694f4380d9fc74eb0a96a9e917d5f1dd4e
ZB <80359447+ZaraB-hub@users.noreply.github.com>
2024-02-08 22:20:10 +0100

    Time: 2 ms (95.62%), Space: 45 MB (19.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,16 @@
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
 
-        int j;
-        for(int i=0;i<nums.length-1;i++){
-            j=i+1;
-            while(j<nums.length){
-                if(nums[i]+nums[j]==target)
-                    return new int[] {i, j};
-                j++;
+
+        HashMap<Integer, Integer> numToIndex = new HashMap<Integer, Integer>();
+        
+         for (int i = 0; i < nums.length; i++) {
+            if (numToIndex.containsKey(target - nums[i])) {
+                return new int[] {numToIndex.get(target - nums[i]), i};
             }
-            }
-        return null;
+            numToIndex.put(nums[i], i);
         }
+        return new int[] {};
+    }
 }
+        
